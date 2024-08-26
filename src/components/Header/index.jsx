b1b84cad2d3874fb39d44d54793be4554838222c
codeInterpreter05 <sarthupadhyay0507@gmail.com>
2024-08-26 22:49:43 +0530

    started dashboard

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,30 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { BsCashCoin } from "react-icons/bs";
 import { MdLogout } from "react-icons/md";
+import { auth } from '../../firebase';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  
-  const handleLogout = () => {
-    console.log('Logout clicked');
+
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user, loading, navigate]);
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      navigate("/auth");
+      toast.success("Logout successful");
+    } catch (error) {
+      console.error('Logout failed:', error);
+      toast.error("Failed to logout");
+    }
   }
 
   return (
-    <div className="sticky top-0 left-0 w-full h-[10vh] flex justify-between items-center bg-[#2970FF] px-3 text-white font-semibold text-2xl">
+    <div className="sticky overflow-y-auto top-0 left-0 w-full h-[10vh] flex justify-between items-center bg-[#2970FF] px-3 text-white font-semibold text-2xl">
       <div className='flex justify-center items-center gap-2'>
         <BsCashCoin className='text-[26px] mt-2' />
         <p className='text-xl sm:text-[22px]'>BudgeBuddy</p>
       </div>
-      <div 
-        className="flex justify-center items-center gap-3 font-medium text-2xl cursor-pointer 
-                  hover:text-gray-300 hover:transition-all hover:duration-300"
-        onClick={handleLogout}
-      >
-        <p className='hidden sm:inline text-xl'>Logout</p>
-        <MdLogout />
-      </div>
+      {
+        user && (
+          <div 
+            className="flex justify-center items-center gap-3 font-medium text-2xl cursor-pointer 
+                      hover:text-gray-300 hover:transition-all hover:duration-300"
+            onClick={handleLogout}
+          > 
+            {/* <img src={user.photoURL} alt="user-image" className='h-10 w-10 rounded-full'/> */}
+            <p className='hidden sm:inline text-xl'>Logout</p>
+            <MdLogout />
+          </div>
+        )
+      }
     </div>
-  )
+  );
 }
 
-export default Header
-
+export default Header;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Cards from '../components/Cards'
 
 const Dashboard = () => {
   return (
-    <div>
-      
+    <div className='h-[90vh] overflow-y-auto no-scrollbar'>
+      <Cards/>
     </div>
   )
 }
