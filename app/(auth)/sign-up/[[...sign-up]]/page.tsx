ce2ad06e12edbd235de8819fc83393525b7daf58
codeInterpreter05 <sarthupadhyay0507@gmail.com>
2024-09-15 23:52:36 +0530

    honojs setup complete

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
   ];
 
   return (
-    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 ">
       <div className="h-full lg:flex flex-col items-center justify-center px-4">
         <div className="text-center space-y-4">
           <div className="flex items-center justify-center mt-4 lg:mt-0">
