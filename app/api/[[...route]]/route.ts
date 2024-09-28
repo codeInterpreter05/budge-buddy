46145aef197da107ff5a72fd274d5f034e3ca27c
codeInterpreter05 <sarthupadhyay0507@gmail.com>
2024-09-28 13:09:35 +0530

    transactions api completed

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -7,6 +7,7 @@ import { HTTPException } from "hono/http-exception"
 
 import accounts from "./accounts"
 import categories from "./categories"
+import transactions from "./transactions"
 
 export const runtime = 'edge'
 
@@ -23,6 +24,7 @@ const app = new Hono().basePath('/api')
 const routes = app
     .route("/accounts", accounts)
     .route("/categories", categories)
+    .route("/transactions", transactions)
 
 export const GET = handle(app)
 export const POST = handle(app)
