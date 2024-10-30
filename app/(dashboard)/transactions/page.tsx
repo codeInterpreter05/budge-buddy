ea82d0f2d6dcf9167d15261d1b6e5d66021c0b27
codeInterpreter05 <sarthupadhyay0507@gmail.com>
2024-10-30 12:31:23 +0530

    started transaction upload feature

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -14,8 +14,39 @@ import { DataTable } from "@/components/data-table";
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { errorToJSON } from "next/dist/server/render";
+import { useState } from "react";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+
+enum VARIANTS {
+    LIST = "LIST",
+    IMPORT = "IMPORT",
+};
+
+const INITIAL_IMPORT_RESULTS = {
+    data: [],
+    errors: [],
+    meta: {}
+}
 
 const TransactionsPage = () => {
+
+    const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+    const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+    const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+        console.log({results});
+        setImportResults(results);
+        setVariant(VARIANTS.IMPORT);
+        
+    }
+
+    const onCancelImport = () => {
+        setImportResults(INITIAL_IMPORT_RESULTS);
+        setVariant(VARIANTS.LIST);
+    }
+
     const newTransaction = useNewTransaction();
     const TransactionsQuery = useGetTransactions();
     const transactions = TransactionsQuery.data || [];
@@ -42,6 +73,18 @@ const TransactionsPage = () => {
         )
     }
 
+    if(variant === VARIANTS.IMPORT){
+        return (
+            <>
+                <ImportCard
+                    data={importResults.data}
+                    onCancel={onCancelImport}
+                    onSubmit={() => {}}
+                />
+            </>
+        )
+    }
+
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
@@ -49,10 +92,15 @@ const TransactionsPage = () => {
                     <CardTitle className="text-xl line-clamp-1">
                         Transactions History
                     </CardTitle>
-                    <Button size={"sm"} onClick={newTransaction.onOpen} >
-                        <Plus className="size-4 mr-1" />
-                        Add new
-                    </Button>
+                    <div className="flex gap-x-2 items-center">
+                        <Button size={"sm"} onClick={newTransaction.onOpen} >
+                            <Plus className="size-4 mr-1" />
+                            Create
+                        </Button>
+                        <UploadButton
+                            onUpload={onUpload}
+                        />
+                    </div>
                 </CardHeader>
                 <CardContent>
                     <DataTable columns={columns} data={transactions} filterKey="by payee" 
